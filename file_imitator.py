174abbd85f26c65582f04ed8687ed7f6130bb6fe
DevolasRevenge <49737689+DevolasRevenge@users.noreply.github.com>
2023-01-23 00:52:21 -0500

    Update file_imitator.py

--- file_imitator.py
+++ file_imitator.py
@@ -3,6 +3,8 @@ import os.path
 from os import path
 import shutil
 
+#this script was created to help me mass duplicate a barebones/nearly empty efx file and rename it to an entire directory/folder/list of other efx files. Doing so helps me determine which efx aren't affected in-game (process of elimination)
+
 #turn this on if you want to only duplicate files with a specific extension
 file_ext_lock = True
 
@@ -36,10 +38,12 @@ output_dir = current_dir + "\\Output_Immitation"
 folder_check(input_dir)
 folder_check(output_dir)
 
+#specify the single file that is to be duplicated and renamed
 base_file = input("Enter the name of a file that you would like to use as a base (must be in the same folder as file_imitator.py): \n")
 
 file_list = []
 
+#adds every file from the input directory to a list
 batch_files = os.listdir(stripper(input_dir))
 
 #files from the set directory are added to a list
@@ -52,6 +56,7 @@ else:
     for f in batch_files:
         file_list.append(f)
 
+#immitates (duplicates) files from the list
 print("\nimmitating files... \n\n")
 for n in file_list:
     new_output = (output_dir + "\\" + n)
